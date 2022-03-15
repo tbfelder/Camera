3d6e9dd2986f99111ecb3ff17041293bf8c33ef6
ape03 <a@b.de>
2022-03-15 15:36:38 +0000

    1

--- src/main/java/com/develogical/camera/Camera.java
+++ src/main/java/com/develogical/camera/Camera.java
@@ -2,12 +2,18 @@ package com.develogical.camera;
 
 public class Camera {
 
+    private final Sensor sensor;
+
+    public Camera(Sensor sensor) {
+        this.sensor = sensor;
+    }
+
     public void pressShutter() {
         // not implemented
     }
 
     public void powerOn() {
-        // not implemented
+        sensor.powerUp();
     }
 
     public void powerOff() {
--- src/test/java/com/develogical/camera/CameraTest.java
+++ src/test/java/com/develogical/camera/CameraTest.java
@@ -8,6 +8,12 @@ import static org.mockito.Mockito.*;
 public class CameraTest {
     @Test
     public void switchingTheCameraOnPowersUpTheSensor() {
-        // write your test here
+        Sensor sensor = mock(Sensor.class);
+
+        Camera underTest = new Camera(sensor);
+
+        underTest.powerOn();
+
+        verify(sensor).powerUp();
     }
 }
